7d7ac091301d94931688e2fe7b18405df85cd7e3
Jorge Garcia <jorge.gcia.gcia@gmail.com>
2017-10-11 12:50:28 +0200

    First Version

---
+++ RmsAdapter/RmsAdapter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Sdt.RMS
+{
+    public class RmsAdapter
+    {
+        const string dllPath = @"C:\Program Files\Cliente 2.1 de Active Directory Rights Management Services\msipc.Dll";
+        [DllImportAttribute(dllPath, EntryPoint = "IpcfGetSerializedLicenseFromFile", CallingConvention = CallingConvention.Cdecl)]
+        public static extern UInt32 IpcfGetSerializedLicenseFromFile(IntPtr wszInputFilePath, [Out] out IntPtr ppvLicense);
+        [DllImportAttribute(dllPath, EntryPoint = "IpcFreeMemory", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void IpcFreeMemory( IntPtr pointer);
+        [DllImportAttribute(dllPath, EntryPoint = "IpcGetErrorMessageText", CallingConvention = CallingConvention.Cdecl)]
+        public static extern UInt32 IpcGetErrorMessageText(UInt32 hrError, Int32 dwLanguageId, [Out] out IntPtr ppwszErrorMessageText);
+
+        private static IntPtr GetIntPtrFromString (string str)
+        {
+            return Marshal.StringToHGlobalUni(str);
+        }
+        private static String GetStringFromIntPtr(IntPtr ptr)
+        {
+            return Marshal.PtrToStringUni(ptr);
+        }
+
+        public static UInt32 GetSerializedLicenseFromFile (string fileName)
+        {
+            IntPtr buffer = new IntPtr();
+            IntPtr ptrFileName = GetIntPtrFromString(fileName);
+            string muestra = GetStringFromIntPtr(ptrFileName);
+            UInt32 result =  IpcfGetSerializedLicenseFromFile(ptrFileName, out buffer);
+            IpcFreeMemory(buffer);
+            return result;
+        }
+        public static string GetErrorMessage (UInt32 errorCode)
+        {
+            IntPtr buffer = new IntPtr();
+            IpcGetErrorMessageText(errorCode,1033,out buffer);
+            string errorMessage = GetStringFromIntPtr(buffer);
+            if (errorMessage == null)
+                errorMessage = new Win32Exception((int) errorCode).Message;
+                return errorMessage; 
+        }
+    }
+}
---
+++ RmsConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sdt.RMS;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace RmsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UInt32 errorCode = RmsAdapter.GetSerializedLicenseFromFile(@"C:\Temp\p.docx");
+            string message = RmsAdapter.GetErrorMessage(errorCode);
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
